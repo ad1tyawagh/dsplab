c815049a42a0966d1288c9ac7fe95713d33e59bc
Aditya Wagh <wagh.adi02@gmail.com>
2019-02-28 15:08:01 +0530

    Correction regarding filter coefficients

--- Lab7/task2.m
+++ Lab7/task2.m
@@ -11,15 +11,15 @@ freqz(num, den);
 % Design a second order IIR bandpass filter with center frequency 0.4?
 % and 3-dB bandwidth of 0.1?. Use Matlab function iirpeak.
 
-iirBandPass = iirpeak(0.4, 0.1);
-freqz(iirBandPass,1);
+[num, den] = iirpeak(0.4, 0.1);
+freqz(num, den);
 % Design a second order IIR bandstop filter with center frequency 0.4?
 % and 3-dB bandwidth of 0.1?. Use Matlab function iirnotch.
 
-iirBandStop = iirnotch(0.4, 0.1);
-freqz(iirnotch,1);
+[num, den] = iirnotch(0.4, 0.1);
+freqz(num, den);
 % Design a comb filter with a 3-dB bandwith of 0.2?. Use Matlab
 % function iircomb.
 
-filterCoeff = iircomb(1, 0.2);
-freqz(iircomb,1);
+[hum,den] = iircomb(1, 0.2);
+freqz(num,den);
